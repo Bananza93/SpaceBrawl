344bee4429bc940f365d6bafbe753945caa6d038
Bananza93 <zubryaev.sergey@gmail.com>
2021-09-05 22:17:53 +0400

    Lesson 3 HW

--- core/src/ru/geekbrains/sprites/Aim.java
+++ core/src/ru/geekbrains/sprites/Aim.java
@@ -9,31 +9,11 @@ import ru.geekbrains.math.Rect;
 
 public class Aim extends Sprite {
 
-    //private static final int AIM_WIDTH = 160;
-    //private static final int AIM_HEIGHT = 160;
-
     public Aim(Texture region) {
         super(new TextureRegion(region));
-        //regions[0].setRegionHeight(AIM_HEIGHT);
-        //regions[0].setRegionWidth(AIM_WIDTH);
         this.setLeft(-0.28f);
         this.setBottom(-0.25f);
         this.setHeight(0.1f);
         this.setWidth(0.1f);
     }
-
-    @Override
-    public void resize(Rect worldBounds) {
-        //this.pos.set(new Vector2(-0.28f, -0.25f));
-        //setHeightProportion(worldBounds.getHeight());
-    }
-
-    @Override
-    public void setHeightProportion(float height) {
-        setHeight(height);
-        float aspect = regions[frame].getRegionWidth() / (float) (regions[frame].getRegionHeight());
-        setWidth(height * aspect);
-    }
-
-
 }
